7e71e17017e65d292b56934862a02548c2884535
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2010-02-11 14:34:59 -0500

    Refactored the if, elsif to a case statement.

--- lib/facebook_distance_of_time_in_words.rb
+++ lib/facebook_distance_of_time_in_words.rb
@@ -3,7 +3,8 @@ module FacebookDistanceOfTimeInWords # :nodoc: all
     def facebook_distance_of_time_in_words(past_date)
       today = Date.today
       days_difference = (today-past_date.to_date).to_i
-      if days_difference == 0
+      case 
+      when days_difference == 0
         #: if the date is today, we display time
         time_difference = (Time.now-past_date)
         if time_difference > 1.hour
@@ -11,10 +12,10 @@ module FacebookDistanceOfTimeInWords # :nodoc: all
         else
           t(:today_at_time, :scope => :facebook_distance_of_time_in_words, :hour => l(past_date, :format => :fdotin_hour_minute))
         end
-      elsif days_difference == 1
+      when days_difference == 1
         #: If the date is yesterday
         t(:yesterday_at_time, :scope => :facebook_distance_of_time_in_words, :hour => l(past_date, :format => :fdotin_hour_minute))
-      elsif days_difference < 7
+      when days_difference < 7
         #: If the date is in the week, we display the day's name
         t(:day_with_hour, :scope => :facebook_distance_of_time_in_words, :day => l(past_date, :format => :fdotin_day_name), :hour => l(past_date, :format => :fdotin_hour_minute))
       else
